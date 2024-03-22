59ea75d3034c8c69c8a8ac232995bb49a3516290
Amani Farid <afari094@uottawa.ca>
2024-03-21 23:27:03 -0400

    primary key for captures

---
+++ capture_dimension.sql
@@ -0,0 +1,27 @@
+CREATE TABLE IF NOT EXISTS public."CaptureDimension"(
+	"CaptureName" character varying, 
+	"StartTime" timestamp, 
+	"EndTime" timestamp, 
+	"Duration" double precision, 
+	"NumHalfOpenConnections" bigint,
+	"SYNCount" bigint 
+	"NumARPRequestsSent" bigint
+);
+
+INSERT INTO public."CaptureDimension"("CaptureName", "StartTime", "EndTime", "Duration", "NumHalfOpenConnections", "SYNCount", "NumARPRequestsSent")
+SELECT
+    "CaptureName",
+    TO_TIMESTAMP(MIN("EpochTime")) as "StartTime",
+    TO_TIMESTAMP(MAX("EpochTime")) as "EndTime",
+    MAX("EpochTime") - MIN("EpochTime") as "Duration",
+    COUNT(DISTINCT CASE WHEN LOWER("SYNFlag") = 'set' AND LOWER("ACKFlag") = 'not set' THEN ("SrcIP", "DstIP", "SrcPort", "DstPort") END) as "NumHalfOpenConnections",
+    COUNT(CASE WHEN Lower("SYNFlag") = 'set' THEN 1 END) as "SYNCount",
+    COUNT(CASE WHEN LOWER("Protocol") = 'arp' AND (LOWER("DstMAC") = 'broadcast' OR "DstOUI" = 'ff:ff:ff') THEN 1 END) as "NumARPRequestsSent"
+FROM
+    public."PacketOutput"
+GROUP BY
+    "CaptureName"
+;
+
+ALTER TABLE public."CaptureDimension"
+ADD COLUMN "CaptureKey" SERIAL PRIMARY KEY;
