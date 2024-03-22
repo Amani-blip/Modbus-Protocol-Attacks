d8e2f2c79d673aae4c66aa344dd14d95b5e8127c
MehdiRih <midoxbeti@gmail.com>
2024-03-22 01:52:42 -0400

    adding SourceDimension

---
+++ SourceDimension.sql
@@ -0,0 +1,28 @@
+CREATE TABLE IF NOT EXISTS public."SourceDimension" (
+    "SrcKey" SERIAL PRIMARY KEY,
+    "SrcIP" character varying,
+    "SrcMAC" character varying,
+    "SrcMACResolved" character varying,
+    "SrcOUI" character varying,
+    "SrcOUIResolved" character varying,
+    "SrcPort" double precision
+);
+
+INSERT INTO public."SourceDimension"(
+    "SrcIP", 
+    "SrcMAC", 
+    "SrcMACResolved", 
+    "SrcOUI", 
+    "SrcOUIResolved", 
+    "SrcPort", 
+    "HardwareSrcAddr"
+)
+SELECT DISTINCT
+    "SrcIP", 
+    "SrcMAC", 
+    "SrcMACResolved", 
+    "SrcOUI", 
+    "SrcOUIResolved", 
+    "SrcPort", 
+FROM
+    public."PacketOutput";
